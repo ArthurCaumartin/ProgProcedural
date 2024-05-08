2e7e15b5ea7629cccdca6ad2e806d3b3a86dcf20
Arthur Caumartin <artcaumart@gmail.com>
2024-05-08 11:23:10 +0200

    gfdtdf

---
+++ Assets/[Project]/Scripts/Mob/MobBehavior.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobBehavior : MonoBehaviour
+{
+    private Transform _target;
+
+    void Update()
+    {
+        if(!_target)
+            return;
+        Vector3 forwardTarget = (_target.position - transform.position).normalized;
+        transform.forward = Vector3.Lerp(transform.forward, forwardTarget, Time.deltaTime * .5f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != "Player")
+            return;
+        
+        GetComponent<SphereCollider>().radius = 1;
+        _target = other.transform;
+    }
+}
